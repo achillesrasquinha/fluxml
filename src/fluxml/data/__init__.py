d3e45e61b397c01fe0cbbcfa5c5ae512fb154fe0
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-10-13 11:16:48 -0500

    feat: fetch bigg models

--- src/fluxml/data/__init__.py
+++ src/fluxml/data/__init__.py
@@ -1,10 +1,15 @@
 import os.path as osp
+from functools import partial
 
 from fluxml.config import PATH
 from fluxml import __name__ as NAME
 
 from bpyutils.util.environ import getenv
 from bpyutils.util.system  import makedirs
+from bpyutils.util.types   import lmap
+from bpyutils              import parallel
+
+from bioservices import BiGG
 
 _PREFIX = NAME.upper()
 
@@ -17,9 +22,26 @@ def get_data_dir(data_dir = None):
 
     return data_dir
 
+def _download_bigg_model(model_id, data_dir = None):
+    data_dir = get_data_dir(data_dir = data_dir)
+    target   = osp.join(data_dir, "%s.xml.gz" % model_id)
+
+    bigg     = BiGG()
+    bigg.download(model_id, format_='xml', target = target)
+
 def get_data(data_dir = None):
     data_dir = get_data_dir(data_dir)
-    # do something ...
+    
+    bigg   = BiGG()
+    models = lmap(lambda x: x["bigg_id"], bigg.models)
+    
+    with parallel.no_daemon_pool() as pool:
+        pool.map(
+            partial(
+                _download_bigg_model,
+                **dict(data_dir = data_dir)
+            )
+        , models)
 
 def preprocess_data(data_dir = None):
     data_dir = get_data_dir(data_dir)
